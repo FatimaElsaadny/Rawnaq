f3d06d8f2549d0919fce17de3046390a9f1321a2
Fatima Elsaadny <fatima.elsaadny@gizasystems.local>
2025-11-08 16:15:39 +0200

    before fetch maraim changes

---
+++ README.md
@@ -0,0 +1,2 @@
+Enhancement
+1- replace  all absolute path in index.html with relative.
---
+++ src/js/config.js
@@ -0,0 +1,3 @@
+// Global configuration for Rawnaq site
+const BASE_PATH = '';
+const SRC_PATH = BASE_PATH + '/src';
